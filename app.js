379c26e4c20bc57ccd272a0d35d7b10840cd8dbf
Soheil Hemmasi <s.hemasi63@gmail.com>
2022-04-26 16:45:20 +0430

    last

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
-// $(document).ready(function() {
-//     $(window).scroll(function() {
-//       if ($(document).scrollTop() > 50) {
-//         $(".fixed-top").addClass("bg-white");
-//       } else {
-//         $(".fixed-top").removeClass("bg-white");
-//       }
-//     });
-//   });
+      const navbar = document.querySelector('.fixed-top')
+document.addEventListener("DOMContentLoaded", function() {
 
- const navbar = document.querySelector('.fixed-top')
-  document.addEventListener("DOMContentLoaded", function() {
-
-    const myFunction=()=>{
-       if(window.scrollY>50){
-          navbar.classList.add("bg-white");
-       }
-       else{
-        navbar.classList.remove("bg-white");
-       }
+ const myFunction=()=>{
+    if(window.scrollY>50){
+       navbar.classList.add("bg-white");
     }
+    else{
+     navbar.classList.remove("bg-white");
+    }
+ }
 
-    window.addEventListener("scroll", myFunction);
-     
+ window.addEventListener("scroll", myFunction);
   
-  });
+
+});
+
+  
+
+ 
